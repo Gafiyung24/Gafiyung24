7fab803db77c98cd030dbce949110ba902a00381
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-10 16:22:32 +0000

    Fri, Nov 10, 2023, 5:22 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,4 @@
-x = input("What is the Answer to the Great Question of Life, the universe, and everything? ").strip().
+x = input("What is the Answer to the Great Question of Life, the universe, and everything? ").strip().lower()
 if x == "42" or x == "forty-two" or x == "forty two":
     print("Yes")
 else:
