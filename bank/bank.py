cad35b96ae9cbea73ea5a8182daf40412087ced1
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-10 17:34:10 +0000

    Fri, Nov 10, 2023, 6:34 PM +01:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,5 +1,5 @@
 x = input("Greeting: ").lower().strip()
-if x == "hello" or "hello" in x == True:
+if x == "hello" or "hello" in x is True:
     print("$0")
 elif x.find("h") == 0 and x != "helllo":
     print("$20")
