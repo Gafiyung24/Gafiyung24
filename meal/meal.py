d5bd50a834beaf15271a7063e5cc2c27b0ccabdd
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 22:57:56 +0000

    Sat, Nov 11, 2023, 11:57 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,9 @@
 def main():
     #collecting user input and storing in variable T
     T = input("What time is it? ").strip()
-    
+    T1 = convert(T)
+    #condtional statement to know meals
+    if 
 
 
 def convert(time):
