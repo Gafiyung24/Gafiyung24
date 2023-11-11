f9a944517d84d6db74e981c4b67a97a1f7be1173
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 20:55:20 +0000

    Sat, Nov 11, 2023, 9:55 PM +01:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -6,5 +6,19 @@ Y = int(E[2])
 # condtional statement to make calculations
 if E[1] == "+":
     Z = X + Y
-    
-
+    Z = float(Z)
+    print(f"{Z: 1f}")
+elif E[1] == "-":
+    Z = X - Y
+    Z = float(Z)
+    print(f"{Z: 1f}")
+elif E[1] == "*":
+    Z = X * Y
+    Z = float(Z)
+    print(f"{Z: 1f}")
+elif E[1] == "/":
+    Z = X / Y
+    Z = float(Z)
+    print(f"{Z: 1f}")
+else:
+    print("This operation is not recognised")
