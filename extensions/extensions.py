ac7d39f6dd791253c38c5ef017acc4323dcb05fc
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 09:27:03 +0000

    Sat, Nov 11, 2023, 10:27 AM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,19 +4,15 @@ x = input("File name: ").strip().lower()
 #checking error incase . is not in entry
 if x.find(".") == -1:
     print("application/octet-stream")
+
 #getting substring for file extension
-x1 = x.split(".")[1].strip()
-
-
-
+else:
+    x1 = x.split(".")[1].strip()
 # condtional statement to find the type of file extensions
-match x1:
-    case "gif" | "jpg" | "jpeg" | "png":
-        print(f"image/{x1}")
-    case "pdf" | "txt" | "zip":
-        print(f"application/{x1}")
-    case _:
-        print("application/octet-stream")
-
-
-
+    match x1:
+        case "gif" | "jpg" | "jpeg" | "png":
+            print(f"image/{x1}")
+        case "pdf" | "txt" | "zip":
+            print(f"application/{x1}")
+        case _:
+            print("application/octet-stream")
