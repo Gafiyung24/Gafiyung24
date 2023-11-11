7ead6e3d31f7d4a6d20f15353c0ca9685275f2c5
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 12:03:26 +0000

    Sat, Nov 11, 2023, 1:03 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -9,6 +9,11 @@ elif x.count(".") == 2:
      x = x.split(".")[1].strip()
      x1 = x.split(".")[1].strip()
 
+#getting substring for file extension
+else:
+    x1 = x.split(".")[1].strip()
+
+# condtional statement to find the type of file extensions
 match x1:
     case "gif" | "jpeg" | "png":
         print(f"image/{x1}")
@@ -18,17 +23,3 @@ match x1:
         print("text/plain")
     case _:
         print("application/octet-stream")
-#getting substring for file extension
-else:
-    x1 = x.split(".")[1].strip()
-
-# condtional statement to find the type of file extensions
-    match x1:
-        case "gif" | "jpeg" | "png":
-            print(f"image/{x1}")
-        case "pdf" | "zip":
-            print(f"application/{x1}")
-        case "txt":
-            print("text/plain")
-        case _:
-            print("application/octet-stream")
