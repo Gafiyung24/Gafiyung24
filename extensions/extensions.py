381f8dcc300b696ae5595ec7285b35c3c824d542
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 10:13:35 +0000

    Sat, Nov 11, 2023, 11:13 AM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -10,4 +10,10 @@ else:
     x1 = x.split(".")[1].strip()
 
 # condtional statement to find the type of file extensions
-    
+match x1:
+        case "gif" | "jpeg" | "png":
+            print(f"image/{x1}")
+        case "pdf" | "txt" | "zip":
+            print(f"application/{x1}")
+        case _:
+            print("application/octet-stream")
