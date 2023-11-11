9a3800ad20583edab94f921ad9d3ad33e2381676
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 22:49:21 +0000

    Sat, Nov 11, 2023, 11:49 PM +01:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,14 @@
 def main():
-    ...
+    
 
 
 def convert(time):
     hours, minutes = time.split(":")
+    hours = float(hours)
+    minutes = float(minutes) / 60
+    time_1 = hours + minutes
+    return time_1
+
 
 
 
