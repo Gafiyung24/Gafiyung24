3ecb3b2271d99bb03946f1f82d2eb4b885622729
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-11 09:22:15 +0000

    Sat, Nov 11, 2023, 10:22 AM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,6 +1,13 @@
 # initialising variable to receive user input and create substring for file extension
 x = input("File name: ").strip().lower()
+
+#checking error incase . is not in entry
+if x.find('.') == -1:
+    print("application/octet-stream")
+#getting substring for file extension
 x1 = x.split(".")[1].strip()
+
+
 
 # condtional statement to find the type of file extensions
 match x1:
@@ -10,6 +17,6 @@ match x1:
         print(f"application/{x1}")
     case _:
         print("application/octet-stream")
-        
+
 
 
