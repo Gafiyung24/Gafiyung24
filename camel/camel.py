49158f70da40d2a8c25de4216ba33fc862a4b84a
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-12 16:38:53 +0000

    Sun, Nov 12, 2023, 5:38 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,4 +1,5 @@
 def main():
+    x = input()
 
     snake_style()
 #function to convert from camel style to snake style
@@ -6,4 +7,4 @@ def snake_style(user_input):
     for i in user_input:
         if user_input[i].isupper() is True:
             output = user_input[:i-1] + "_"
-            
+    return output
