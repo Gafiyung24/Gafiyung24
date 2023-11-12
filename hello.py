0c3d4c9e46f3597882ff8a6029ac3125651b2d01
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-12 10:25:52 +0000

    Sun, Nov 12, 2023, 11:25 AM +01:00

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 """input("What is your name? :")
-print("hello, World")"""
-print("meow\n" * 3, end= "")
+print("hello, World")
+print("meow\n" * 3, end= "")"""
+def main():
+    n = print_column(3)
+    print(f'{n}*3')
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+
+main()
