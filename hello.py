bf67aca48eafa73d84706c1e9114beb5472d1d5e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-12 22:16:40 +0000

    Sun, Nov 12, 2023, 11:16 PM +01:00

--- hello.py
+++ hello.py
@@ -22,7 +22,6 @@ def print_square(size):
 main()"""
 user_input = input("name: ")
 for i, u in enumerate(user_input):
-    output = user_input[0].lower()
     if u.isupper():
             output += "_" + u.lower()
             # output = user_input[:i] + "_" + u.lower()
