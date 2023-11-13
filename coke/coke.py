da4ac0d19760f24f022d198217d460522ffbb086
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-13 19:13:08 +0000

    Mon, Nov 13, 2023, 8:13 PM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,10 @@
 #defining price of coke
 def main():
-    amount_due = 50
+    n = 50
     calc_change()
 
 def calc_change(amount_due):
-    while
+    while 
 
     n = int(input("Insert Coin: "))
 
