97f8a128572a4cfd45fb5ac2a3e716a6c8b77a85
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-13 23:22:02 +0000

    Tue, Nov 14, 2023, 12:22 AM +01:00

--- coke/coke.py
+++ coke/coke.py
@@ -9,7 +9,7 @@ def calc_change():
     while i < 50:
         n = int(input("Insert Coin: "))
 
-        if n == 5 | 10 | 25:
+        if n == 5 or 10 or 25:
             i = n + i
             if i < 50:
                 print("Amount Due:", 50-i)
