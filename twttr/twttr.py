187429e3b1bc7f4800c4d99116ddb964b8ab9c06
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-14 09:18:58 +0000

    Tue, Nov 14, 2023, 10:18 AM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -5,5 +5,7 @@ def main():
 def remove_vowels():
     n = input("Input: ")
 #dictionary to contain vowels and an empty string
-    v = {"a": "", }
+    v = {"a":"", "e":"", "i":"", "o":"", "u":"" }
+#for loop to iterate through the string
+    for 
 
