a6077377f8018e1ab8c8c393e6c16329f2f5cce3
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-14 15:14:17 +0000

    Tue, Nov 14, 2023, 4:14 PM +01:00

--- plates.py
+++ plates.py
@@ -7,22 +7,27 @@ def main():
 
 
 def is_valid(s):
-    if 
+    if check_alpha(s) and check_len(s) and check_last(s) and check_alnum():
+        return True
+
 def check_alpha(s):
     #function to check if first 2 digits are alphabets
     for i, c in enumerate(s):
         if s[0].isalpha() and s[1].isalpha():
             return True
+
 def check_len(s):
     #fuction to check if length of plate is within 2 and 6 digits
     p = len(s)
     if 2 <= p <= 6:
         return True
+
 def check_last(s):
     #fuction to make suure the last digit is a number
     for i, c in enumerate(s):
         if s[i].isnumeric() and s[i+1:].isnumeric():
             return True
+
 def check_alnum(s):
     if s.isalnum():
         return True
