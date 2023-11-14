b7cfe531cd8bacb1f7998ca54200b76a2ea3d46b
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-14 18:47:33 +0000

    Tue, Nov 14, 2023, 7:47 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -15,7 +15,7 @@ def check_alpha(s):
     p = len(s)
     if 2 <= p <= 6:
         for i, c in enumerate(s):
-            elif s[0].isalpha() and s[1].isalpha():
+            if s[0].isalpha() and s[1].isalpha():
                 a = True
             else:
                 a = False
