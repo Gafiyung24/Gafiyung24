97e6c5042bcb5fad965725552662074cce942273
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-14 23:32:41 +0000

    Wed, Nov 15, 2023, 12:32 AM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -11,8 +11,8 @@ def is_valid(s):
         t = True
     elif check_all_alpha(s) and check_len(s):#for all alphabets
         t = True
-    """elif check_alpha(s) and check_len(s) and first_num_zero(s):
-        t = False"""
+    elif check_alpha(s) and check_len(s) and first_num_zero(s):
+        t = False
     else:
         t = False
     return t
