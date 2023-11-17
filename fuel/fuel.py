e9c817edea297ca5c64fe84dd33e8e22622f755e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-17 22:49:34 +0000

    Fri, Nov 17, 2023, 11:49 PM +01:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,13 +1,21 @@
 def main():
+    l = get
 
 
 
 
 
 
-    def get_level():
-        while True:
-            x = input("Fraction: ")
+
+def get_level():
+    while True:
+        x = input("Fraction: ").split("/")
+        try:
+            return int(x[0])/int(x[1])
+        except (ValueError, ZeroDivisionError)
+            pass
+
+
 
 main()
 
