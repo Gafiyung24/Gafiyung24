6d1f415cc91c2b22a45d91bf2bf915613d6b133a
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-18 14:29:56 +0000

    Sat, Nov 18, 2023, 3:29 PM +01:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -21,7 +21,7 @@ def get_order(menu):
     items = []
     while True:
         try:
-            items.append(get(input("Item: ").capitalise()[menu]))
+            items.append(get(input("Item: ").capitalise()[menu])))
 
 
 
