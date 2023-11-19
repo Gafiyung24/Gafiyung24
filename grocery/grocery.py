8a5ba5c74c5a37007af027abd500155b49f9489b
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-19 21:02:42 +0000

    Sun, Nov 19, 2023, 10:02 PM +01:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,10 +1,17 @@
-def main();
+def main():
+
 
 def list():
+
     my_list = []
     while True:
         try:
             my_list.append(input().upper())
+        except KeyError:
+            pass
+        except EOFError:
+            return my_list.sort()
+
 
 
 main()
