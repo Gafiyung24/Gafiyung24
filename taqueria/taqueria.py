4fde2fd6d417ab993e8df898353e04a7a2b648a3
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-19 00:27:32 +0000

    Sun, Nov 19, 2023, 1:27 AM +01:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -24,7 +24,7 @@ def get_order(menu): #function to get user input and handle errors
             pass
 
         except EOFError:
-            return items
+            return
 
 def total(items): #function to get total of items picked
     t = sum(items)
