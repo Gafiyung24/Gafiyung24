2f0d1c90d501532748a6b028ec5f110d8e5cfeab
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-20 19:04:15 +0000

    Mon, Nov 20, 2023, 8:04 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -64,6 +64,7 @@ def convert_date():
                 pass
             else:#printin out date
                 print(f"{year}-{m+1:02}-{d:02}")
+                break
         elif month in months_ab:#checking if month wasn't fully spelt
             try:
                 d = days_m.index(day)
@@ -72,9 +73,17 @@ def convert_date():
                 pass
             else:#printin out date
                 print(f"{year}-{m+1:02}-{d:02}")
+                break
         elif month in month_n:
             try:
                 m = month_n.index(month)
+                d = days_m.index(day)
+            except ValueError:
+                pass
+            else:
+                print(f"{year}-{m:02}-{d:02}")
+                break
+
 
 
 
