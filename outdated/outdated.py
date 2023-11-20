34ae94f9db7ff5fcac3435e5c47a553dd71fa99e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-20 23:21:54 +0000

    Tue, Nov 21, 2023, 12:21 AM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -68,7 +68,7 @@ while True:
                     print(f"{year}-{m+1:02}-{d:02}")
                     break
 
-
+            
         for i in months_ab:
             if month in months_ab:#checking if month wasn't fully spelt
                 day = int(day)
@@ -80,7 +80,7 @@ while True:
                 else:#printin out date
                     print(f"{year}-{m+1:02}-{d:02}")
                     break
-        
+
 
         if month.isnumeric() and day.isnumeric():
             month = int(month)
