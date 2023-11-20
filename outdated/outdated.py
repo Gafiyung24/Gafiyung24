2b93b3edc76f7d88e8bcaeb7f3a4ccaef11b2b67
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-20 21:10:09 +0000

    Mon, Nov 20, 2023, 10:10 PM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -32,64 +32,64 @@ months_ab = [
 ]
 month_n = range(12)
 days_m = range(31)
-"""while True:"""
-user_input = input("Date: ") #taking user's date input
-if user_input.find("/") != -1:#handling dates in the format 8/9/1996
-    user_input = user_input.split("/")
-    month = user_input[0]
-    day = user_input[1]
-    year = user_input[2]
-elif user_input.find(",") != -1:#handling dates with month spellings and comma
-    user_input = user_input.split(", ")
-    u1 = user_input[0].split(" ")[0]
-    u2 = user_input[0].split(" ")[1]
-    year = user_input[2]
-    if u1.isnumeric():
-        day = u1
-        month = u2
-    else:
-        month = u1
-        day = u2
-
-elif user_input.find(" ") != -1:#handling dates with months spelling without comma
-    user_input = user_input.split(" ")
-    day = user_input[0]
-    month = user_input[1]
-    year = user_input[2]
-for i in months_a:
-    if month in months_a:#checking if month was fully spelt
-        day = int(day)
-        try:
-            d = days_m.index(day)
-            m = months_a.index(month)
-        except ValueError:
-            pass
-        else:#printin out date
-            print(f"{year}-{m+1:02}-{d:02}")
-    #break
-for i in months_ab:
-    if month in months_ab:#checking if month wasn't fully spelt
-        day = int(day)
-        try:
-            d = days_m.index(day)
-            m = months_ab.index(month)
-        except ValueError:
-            pass
-        else:#printin out date
-            print(f"{year}-{m+1:02}-{d:02}")
-    #break
-for i in month_n:
-    month = int(month)
-    day = int(day)
-    if month == i:
-        try:
-            m = month_n.index(month)
-            d = days_m.index(day)
-        except ValueError:
-            pass
+while True:
+    user_input = input("Date: ") #taking user's date input
+    if user_input.find("/") != -1:#handling dates in the format 8/9/1996
+        user_input = user_input.split("/")
+        month = user_input[0]
+        day = user_input[1]
+        year = user_input[2]
+    elif user_input.find(",") != -1:#handling dates with month spellings and comma
+        user_input = user_input.split(", ")
+        u1 = user_input[0].split(" ")[0]
+        u2 = user_input[0].split(" ")[1]
+        year = user_input[2]
+        if u1.isnumeric():
+            day = u1
+            month = u2
         else:
-            print(f"{year}-{m:02}-{d:02}")
-    #break
+            month = u1
+            day = u2
+
+    elif user_input.find(" ") != -1:#handling dates with months spelling without comma
+        user_input = user_input.split(" ")
+        day = user_input[0]
+        month = user_input[1]
+        year = user_input[2]
+    for i in months_a:
+        if month in months_a:#checking if month was fully spelt
+            day = int(day)
+            try:
+                d = days_m.index(day)
+                m = months_a.index(month)
+            except ValueError:
+                pass
+            else:#printin out date
+                print(f"{year}-{m+1:02}-{d:02}")
+        #break
+    for i in months_ab:
+        if month in months_ab:#checking if month wasn't fully spelt
+            day = int(day)
+            try:
+                d = days_m.index(day)
+                m = months_ab.index(month)
+            except ValueError:
+                pass
+            else:#printin out date
+                print(f"{year}-{m+1:02}-{d:02}")
+        #break
+    for i in month_n:
+        month = int(month)
+        day = int(day)
+        if month == i:
+            try:
+                m = month_n.index(month)
+                d = days_m.index(day)
+            except ValueError:
+                pass
+            else:
+                print(f"{year}-{m:02}-{d:02}")
+        #break
 
 
 
