a4a96422ce9b73b9adacefdd6cd5497ccd3fda9e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-20 16:32:43 +0000

    Mon, Nov 20, 2023, 5:32 PM +01:00

---
+++ outdated/outdated.py
@@ -0,0 +1,34 @@
+def main():
+def convert_date():
+    months_a = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
+    months_ab = [
+
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+    ]
+    month_n = 
+main()
+
