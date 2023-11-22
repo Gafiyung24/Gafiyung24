015e35ac36a13e328ea10148c4903f1f16a02cbc
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-22 23:59:07 +0000

    Thu, Nov 23, 2023, 12:59 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,12 +5,14 @@ import random
 
 figlet = Figlet()
 if len(sys.argv) == 1:
+    user_input = input("Input: ")
     f = figlet.getFonts()
     rf = random.choice(f)
     figlet.setFont(font=rf)
     print(figlet.renderText(user_input))
 
 elif "-f" or "--font" == sys.argv[1]:
+    user_input = input("Input: ")
     figlet.setFont(font = sys.argv[2])
     print(figlet.renderText(user_input))
 elif "-f" or "--font" != sys.argv[1] or sys.argv[2] not in figlet.getFonts():
