6963915c927b32bb5371080bc8b851141e4b3210
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-22 23:34:54 +0000

    Thu, Nov 23, 2023, 12:34 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,1 +1,13 @@
-0
+from pyfiglet import Figlet
+import sys
+import random
+
+user_input = input("Input: ")
+figlet = Figlet()
+if len(sys.argv) == 1:
+    f = figlet.getFonts()
+    rf = random.choice(f)
+    figlet.setFont(font=rf)
+    print(figlet.renderText(user_input))
+
+elif "-f
