2f362fdddf097c20761367383c19fa51ace955c8
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-23 12:27:32 +0000

    Thu, Nov 23, 2023, 1:27 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -13,11 +13,3 @@ if len(sys.argv) == 1:
     figlet.setFont(font=rf)
     print(figlet.renderText(user_input))
 
-elif fig_f or fig_font == sys.argv[1]:
-    user_input = input("Input: ")
-    figlet.setFont(font = sys.argv[2])
-    print(figlet.renderText(user_input))
-elif fig_f != sys.argv[1] or sys.argv[2] not in figlet.getFonts():
-    sys.exit("Invalid usage")
-elif 1 < len(sys.argv) < 3:
-    sys.exit("Invalid usage")
