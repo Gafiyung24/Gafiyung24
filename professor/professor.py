b38c35bcb7ce0b4c4e70fa945097306813d995a6
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-25 23:14:29 +0000

    Sun, Nov 26, 2023, 12:14 AM +01:00

---
+++ professor/professor.py
@@ -0,0 +1,26 @@
+import random
+
+
+def main():
+
+
+
+def get_level():
+    while True:
+        try:
+            n = int(input("Level: "))
+            if n <= 0 or n > 3:
+                raise ValueError
+        except ValueError:
+            pass
+        else:
+            return n
+
+
+def generate_integer(level):
+    if level == 1
+    p = rand
+
+
+if __name__ == "__main__":
+    main()
