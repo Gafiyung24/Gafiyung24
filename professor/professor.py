3b2b739810d33f36d94753fac7ec13c11f2c306e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-25 23:23:17 +0000

    Sun, Nov 26, 2023, 12:23 AM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -5,7 +5,7 @@ def main():
 
 
 
-def get_level():
+def get_level():# take userinput and check for wrong input
     while True:
         try:
             n = int(input("Level: "))
@@ -17,12 +17,20 @@ def get_level():
             return n
 
 
-def generate_integer(level):
+def generate_integer(level):# generate intergers based on level entered
     if level == 1:
-    p = random.randrange(0,9)
-    q = random.randrange(0,9)
+        p = random.randrange(0,9)
+        q = random.randrange(0,9)
+    elif level == 2:
+        p = random.randrange(10,99)
+        q = random.randrange(10,99)
+    elif level == 3:
+        p = random.randrange(100,999)
+        q = random.randrange(100,999)
+    return p, q
 
-    
+
+
 
 
 if __name__ == "__main__":
