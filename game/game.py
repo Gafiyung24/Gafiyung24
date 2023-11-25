ffc46d2e9f3297d1414e955d5dde4ddd14c21d94
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-25 05:55:00 +0000

    Sat, Nov 25, 2023, 6:55 AM +01:00

--- game/game.py
+++ game/game.py
@@ -16,7 +16,7 @@ def check():
             pass
         else:
             c = random.choice(1, n)
-            return c, i
+        return c, i
 def game(a, b):
     if a < b:
         print("Too small")
