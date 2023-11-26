9308c08ffa0d0f639e079c31c3c338a4b2ac007e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-26 22:29:14 +0000

    Sun, Nov 26, 2023, 11:29 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -31,11 +31,11 @@ def problem_solver(Z, X , Y):
                 if x == 3:
                     print(Z)
                     x = 0
-                    break   #code runs with manual checkng but check50 coming up with errors except I use sys.exit instead of break.
+                    sys.exit()   #code runs with manual checkng but check50 coming up with errors except I use sys.exit instead of break.
             else:
                 if Z == us_2:
                     score +=1
-        
+
     return score
 
 
