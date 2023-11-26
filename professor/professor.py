ad2308a6f77f8679e4dbe77e765cec701b541593
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-26 16:51:33 +0000

    Sun, Nov 26, 2023, 5:51 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -21,7 +21,7 @@ def main():
                 x +=1
                 if x == 3:
                     print(Z)
-                    break
+                    x = 0
                 pass
             else:
                 if Z == us_2:
