d37d1992750bc2da5f8353e0b489626f23171a30
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-26 17:16:11 +0000

    Sun, Nov 26, 2023, 6:16 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -15,6 +15,8 @@ def main():
                 us_2 = int(input(" "))
                 if Z != us_2:
                     raise ValueError
+                elif Z == us_2:
+                    score +=1
             except ValueError:
                 print("EEE")
                 print(f"{X} + {Y} = ", end='')
@@ -25,8 +27,6 @@ def main():
                     break
                 pass
             else:
-                if Z == us_2:
-                    score +=1
                 break
         i +=1
     print(f"{score}")
