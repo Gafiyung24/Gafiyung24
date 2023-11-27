53ac86bc83be1e9f9eb9bf300e16db9f9a4379d6
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-27 23:54:49 +0000

    Tue, Nov 28, 2023, 12:54 AM +01:00

---
+++ test_bank/bank.py
@@ -0,0 +1,16 @@
+def main():
+    x = input("Greeting: ")
+    print(value(x))
+
+def value(greeting):
+    greetings = greeting.lower().strip()
+    if "hello" in greetings:
+        value = "$0"
+    elif greetings.find("h") == 0 and "hello" not in greetings :
+        value = "$20"
+    else:
+        value = "$100"
+    return value
+if __name__ == "__main__":
+    main()
+
