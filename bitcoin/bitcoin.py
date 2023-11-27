2297b4999c17dbc5f35d2da4dc971b85de035c64
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-27 17:06:34 +0000

    Mon, Nov 27, 2023, 6:06 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,15 +1,22 @@
 import requests
 import sys
 
-try:#error handling block of the request from coincap
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-except requests.RequestException:
-    print("There is an issue with the request")
+def main():
 
-r = response.json()
-cp = float(r["bpi"["USD"["rate"]]])
+def get_price():
+    #function to get price of btc from coinmarket cap
+    try:#error handling block of the request from coincap
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    except requests.RequestException:
+        print("There is an issue with the request")
+
+    r = response.json()
+    cp = float(r["bpi"["USD"["rate"]]])
+    return cp
 
 
 
+
+main()
 
 
