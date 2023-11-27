cf615090776a1c3b100fe03f883fb41a238f6fd0
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-27 22:17:41 +0000

    Mon, Nov 27, 2023, 11:17 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,15 +1,17 @@
 def main():
-    n_v = shorten()
+
+    n = input("Input: ")
+    n_v = shorten(n)
     print("Output: ", n_v)
 
 #create function remove vowels
-def shorten():
-    n = input("Input: ")
+def shorten(word):
+
 #dictionary to contain vowels and an empty string
     v = {"a":"", "A":"", "E":"",  "e":"", "I":"", "i":"", "O":"", "o":"", "u":"", "U":""}
 #for loop to iterate through the string
     for old, new in v.items():
-        n = n.replace(old, new)
-    return n
+        word = word.replace(old, new)
+    return word
 if __name__ == "__main__":
     main()
