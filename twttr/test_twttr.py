36d11e57a86a1701c5597d0241359da8e12b6653
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-27 22:29:27 +0000

    Mon, Nov 27, 2023, 11:29 PM +01:00

--- twttr/test_twttr.py
+++ twttr/test_twttr.py
@@ -4,3 +4,10 @@ def test_word():
     assert shorten("already") == "lrdy"
     assert shorten("EvangElIsm") == "vnglsm"
 
+def test_numbers():
+    assert shorten("5dream") == "5drm"
+
+def test_punc():
+    assert shorten("Femi.,d") == "Fm.,d"
+    
+
