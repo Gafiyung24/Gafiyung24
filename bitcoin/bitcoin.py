0b605dcf1dae480850af22747ecebacaf7f1703f
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-27 16:51:17 +0000

    Mon, Nov 27, 2023, 5:51 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,7 +1,7 @@
 import request
 import sys
 
-try:# 
+try:#error handling block of the request from coincap
     response = request.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 except request.RequestException:
     print("There is an issue with the request")
