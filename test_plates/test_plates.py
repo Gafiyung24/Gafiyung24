a61cb78ceb510c593d4bf9834df0e0014596c8d8
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 17:10:15 +0000

    Tue, Nov 28, 2023, 6:10 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -9,6 +9,7 @@ def test_alnum():
     assert isvalid("CS50") == True
     assert isvalid("CS05") == False
     assert isvalid("GA55A") == False
+    assert isvalid("50") == False
 def test_mark():
     assert isvalid("HELLO, GOODBYE") == False
-    
+
