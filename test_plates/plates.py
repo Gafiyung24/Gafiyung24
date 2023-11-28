0be8f5ba022c3f09c55097bedbaca80c9d9086b3
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 16:35:35 +0000

    Tue, Nov 28, 2023, 5:35 PM +01:00

---
+++ test_plates/plates.py
@@ -0,0 +1,71 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if check_alpha(s) and check_len(s) and check_last(s) and check_alnum(s) and first_num_zero(s) : #for only alphabet and numbers
+        t = True
+    elif check_all_alpha(s) and check_len(s):#for all alphabets
+        t = True
+    else:
+        t = False
+    return t
+
+def check_alpha(s):
+    #function to check if first 2 digits are alphabets and zero
+    p = len(s)
+    if 2 <= p <= 6:
+        for i, c in enumerate(s):
+            if s[0].isalpha() and s[1].isalpha():
+                a = True
+            else:
+                a = False
+        return a
+
+def check_len(s):
+    #fuction to check if length of plate is within 2 and 6 digits
+    p = len(s)
+    if 2 <= p <= 6:
+        p = True
+    else:
+        p = False
+    return p
+
+
+def check_last(s):
+    #fuction to make suure the last digit is a number
+    if s.isalnum():
+        for i, c in enumerate(s):
+            if s[i].isnumeric() and s[i+1:].isnumeric():
+                return True
+        return False
+
+def check_alnum(s):
+    #check if all digits are alphabets
+    if s.isalnum() is True:
+        return True
+    return True
+
+def check_all_alpha(s):
+    #check if all digits are alphabets
+    if s.isalpha():
+        p = True
+    else:
+        p = False
+    return p
+
+def first_num_zero(s):
+    #check if first number is zero
+    l = s.find("0")
+    for i, c in enumerate(s):
+        if s[l-1].isalpha():
+            return False
+    return True
+
+if __name__ == "__main__":
+    main()
+
