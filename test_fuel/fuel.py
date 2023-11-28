cd39249aaa303dc01f9e5d80dce0422613203acc
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 22:20:06 +0000

    Tue, Nov 28, 2023, 11:20 PM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,3 +1,4 @@
+import sys
 def main():
 
     p = input("Fraction: ") #users to enter fraction here X/Y
@@ -23,7 +24,7 @@ def convert(f):
         try:
             return (int(x[0])/int(x[1]))
         except (ValueError, ZeroDivisionError):
-            pass
+            
 
 
 if __name__ == "__main__":
