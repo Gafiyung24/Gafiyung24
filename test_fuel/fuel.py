b83bc8ee2d513adf4b2bf35387054cddfceddea7
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 23:01:20 +0000

    Wed, Nov 29, 2023, 12:01 AM +01:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -20,7 +20,7 @@ def convert(f):
     x = f.split("/")
     if int(x[1]) == 0:
         raise ZeroDivisionError
-    elif int(x[0]).isnumeric or int(x[1]).isnumeric == False:
+    elif int(x[0]).isnumeric() or int(x[1]).isnumeric() == False:
         raise ValueError
     elif int(x[0]) > int(x[1]):
         raise ValueError
