5b3ff3ab804f9868d3ad283141a933cecb6fb976
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 00:17:57 +0000

    Tue, Nov 28, 2023, 1:17 AM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,10 +2,10 @@ from bank import value
 
 def test_hello():
     assert value("hello Sam") == 0
-    assert value("hello Gaffy") == 0
+    assert value("Hello Gaffy") == 0
 def test_h():
     assert value("hi Sam") == 20
-    assert value("hey Gaffy") == 20
+    assert value("Hey Gaffy") == 20
 def test_others():
     assert value("What's up?") == 100
     assert value("what's happening?") == 100
