887272d1a84207ff4191b721c270b857c49d8eb6
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 17:13:14 +0000

    Tue, Nov 28, 2023, 6:13 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,16 +1,16 @@
 from plates import is_valid
 
 def test_allalpha():
-    assert isvalid("CS") == True
-    assert isvalid("HELLO") == True
-    assert isvalid("GOODBYE") == False
+    assert is_valid("CS") == True
+    assert is_valid("HELLO") == True
+    assert is_valid("GOODBYE") == False
 
 def test_alnum():
-    assert isvalid("CS50") == True
-    assert isvalid("CS05") == False
-    assert isvalid("GA55A") == False
-    assert isvalid("50") == False
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("GA55A") == False
+    assert is_valid("50") == False
 def test_mark():
-    assert isvalid("HELLO, GOODBYE") == False
-    assert isvalid("GA.50") == False
+    assert is_valid("HELLO, GOODBYE") == False
+    assert is_valid("GA.50") == False
 
