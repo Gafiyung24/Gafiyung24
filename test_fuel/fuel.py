fbcc2b52879a35db71b932e3d47010f25c0c11ec
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 18:29:41 +0000

    Tue, Nov 28, 2023, 7:29 PM +01:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,26 @@
+def main():
+    l = get_level() * 100
+
+    if l <= 1:
+        print("E")
+    elif 99 <= l <= 100:
+        print("F")
+    elif 1 < l < 99:
+        print(f"{round(l)}%")
+    else:
+        get_level()
+
+
+
+def get_level():
+    while True:
+        x = input("Fraction: ").split("/")
+        try:
+            return (int(x[0])/int(x[1]))
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+
+main()
+
