5977c64093478af55e7d664dcb83b6d46a985849
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-28 17:04:33 +0000

    Tue, Nov 28, 2023, 6:04 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,7 +1,8 @@
 from plates import is_valid
 
 def test_allalpha():
-    assert isvalid("CS50") == True
+    assert isvalid("CS") == True
     assert isvalid("HELLO") == True
-    assert isvalid("GOODBYE") == True
-    
+    assert isvalid("GOODBYE") == False
+    assert isvalid("GA55A") == False
+
