da173ef72c9270705fef55ac2824573812b3367d
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-11-29 00:23:21 +0000

    Wed, Nov 29, 2023, 1:23 AM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -12,9 +12,7 @@ def test_zerodivision():#test for zerodivisions
     with pytest.raises(ValueError):
         convert("cat/dog")"""
 def test_gauge():
-    assert gauge(0.5) == "E"
     assert gauge(1) == "E"
-    assert gauge(99.5) == "F"
     assert gauge(99) == "F"
     assert gauge(100) == "F"
     assert gauge(50) == "50%"
