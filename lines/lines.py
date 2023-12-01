132bc5ea4aba33d4195266593935e06eac331590
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-01 16:58:29 +0000

    Fri, Dec 1, 2023, 5:58 PM +01:00

---
+++ lines/lines.py
@@ -0,0 +1,29 @@
+import sys
+def main():
+    
+
+def check_arg():
+    #check for condtions of command line argumemts
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line argumrnts")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command_line arguments")
+    elif sys.argv[1].find(".py") == -1:
+        sys.exit("File doesn't exist")
+    return sys.argv[1]
+
+def read(n):
+    count = 0
+    try:
+        with open(n, "r") as file
+    except FileNotFoundError:
+        sys.exit("File doesn't exist")
+    else:
+        lines = file.readlines()
+        for line in lines:
+            if line.strip().startwith("#") == False:
+                count += 1
+        return count
+
+if __name__ = "__main__:
+    main()
