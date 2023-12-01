2e876cbced304d189dd907686c4ef87b1c2d986a
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-01 17:20:05 +0000

    Fri, Dec 1, 2023, 6:20 PM +01:00

---
+++ lines/bitcoin.py
@@ -0,0 +1,36 @@
+import requests
+import sys
+
+def main():
+    c_c = get_price() * check_input()
+    print(f"${c_c:,.4f}")
+
+def get_price():
+    #function to get price of btc from coinmarket cap
+    try:#error handling block of the request from coincap
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    except requests.RequestException:
+        print("There is an issue with the request")
+
+    r = response.json()
+    cp = r["bpi"]["USD"]["rate_float"]
+    return cp
+
+def check_input():
+    #function to check command line argument
+    try:
+        n = float(sys.argv[1])
+        if len(sys.argv) != 2:
+            raise IndexError
+    except IndexError:
+        sys.exit("Missing command-line argument")
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
+    else:
+        return n
+
+
+
+main()
+
+
