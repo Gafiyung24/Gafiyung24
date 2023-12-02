bee65ea0f531f2b3a55086f911343ebf620cb473
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-02 14:24:21 +0000

    Sat, Dec 2, 2023, 3:24 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -21,11 +21,13 @@ def csv_printer():
     try:
         with open("pizza/sicilian.csv", "r") as file:
             lines = csv.DictReader(file)
+            for line in lines:
+                pizzas.append({"Sicilian": line["Sicilian"], "Small": line["Small"], "Large": line["Large"]})
     except FileNotFoundError:
         sys.exit("File doesn't exist")
-    else:
-        for line in lines:
-            pizzas.append({"Sicilian": line["Sicilian"], "Small": line["Small"], "Large": line["Large"]})
+    #else:
+        #for line in lines:
+           # pizzas.append({"Sicilian": line["Sicilian"], "Small": line["Small"], "Large": line["Large"]})
     #for pizza in pizzas:
 
     #print(tabulate(pizzas, headers = "key", tablefmt = "grid"))
