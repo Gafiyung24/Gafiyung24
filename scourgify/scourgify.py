45dbc999b6f68b68bacafdbd65ac601ca4a832ff
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-02 22:55:31 +0000

    Sat, Dec 2, 2023, 11:55 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -17,7 +17,9 @@ def csv_spliter(n):
             for line in lines:
                 names.append(line["name"].split(",")[0], line["house"])
     except FileNotFoundError:
-        sys.exit(f"cannot read {n}" )
+        sys.exit(f"cannot read {n}")
+    else:
+        return names
 
 
 if __name__ == "__main__":
