0bcc2b79fd57434946c7dcb487246ba9f34f8990
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-02 13:07:03 +0000

    Sat, Dec 2, 2023, 2:07 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -4,4 +4,21 @@ import csv
 
 def main():
 
+def check_arg():
+    #check for condtions of command line argumemts
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command_line arguments")
+    elif sys.argv[1].find(".py") == -1:
+        sys.exit("File doesn't exist")
+    return sys.argv[1]
+
+def csv_printer():
     
+
+
+
+
+if __name__ == "__main__":
+    main()
