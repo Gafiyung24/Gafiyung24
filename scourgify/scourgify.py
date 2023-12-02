0ad88779483e4fb551c92238329d287a8f456ff6
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-02 22:15:31 +0000

    Sat, Dec 2, 2023, 11:15 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -4,8 +4,9 @@ import sys
 def main():
 
 def check():#function to check argument
-    if len(sys.argv) != 3:
-        sys.exit("Too )
+    if len(sys.argv) < 3:
+        sys.exit("Too few command line arguments")
+    elif 
 
 if __name__ == "__main__":
     main()
