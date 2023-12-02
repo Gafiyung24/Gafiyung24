64517021acd6785826a673cabf2666d7efd201ef
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-02 16:16:42 +0000

    Sat, Dec 2, 2023, 5:16 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -4,9 +4,12 @@ import csv
 
 def main():
     c = check_arg() #storing sys.argv in a variable after check
-    l = csv_printer(c) #storing table in a variable
-    print(l)
-
+    if sys.argv[1] == "Sicilian.csv":
+        l = csv_printer(c) #storing table in a variable
+        print(l)
+    elif sys.argv[1] == "Regular.csv":
+        l = csv_printer(c) #storing table in a variable
+        print(l)
 
 def check_arg():
     #check for condtions of command line argumemts
@@ -25,6 +28,19 @@ def csv_printer(n):#function to print csv talble
             lines = csv.DictReader(file)
             for line in lines:
                 pizzas.append({"Sicilian Pizza": line["Sicilian Pizza"], "Small": line["Small"], "Large": line["Large"]})
+    except FileNotFoundError:
+        sys.exit("File doesn't exist")
+    else:
+        pizza_table = tabulate(pizzas, headers = "keys", tablefmt = "grid")
+        return pizza_table
+
+def csv_printer(n):#function to print csv talble regular pizza
+    pizzas = []
+    try:
+        with open(n, "r") as file:
+            lines = csv.DictReader(file)
+            for line in lines:
+                pizzas.append({"Regular Pizza": line["Regular Pizza"], "Small": line["Small"], "Large": line["Large"]})
     except FileNotFoundError:
         sys.exit("File doesn't exist")
     else:
