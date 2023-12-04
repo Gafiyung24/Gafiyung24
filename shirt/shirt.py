d908958fb614caed353290cd608c0a550a0ffa4c
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-04 22:52:54 +0000

    Mon, Dec 4, 2023, 11:52 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,14 +3,17 @@ from PIL import Image
 def main():
 
 def check():
-    ext = (".j)
+    ext = (".jpg", "jpeg", "png")
     f1, ext1 = os.path.splitext(sys.argv[1])
     f2, ext2 = os.path.splitext(sys.argv[2])
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
-    elif ext1 not in
+    elif ext1.lower() not in ext and ext2.lower() not in ext:
+        sys.exit("Input and Output have different extensions")
+    elif ext1 != ext2:
+        sys.exit("Unknown file type")
 
 
 if __name__ == "__main__":
