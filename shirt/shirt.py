53a7b4d5c90b88147ec87aac0c4fa5e51d49b9c0
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-04 22:48:36 +0000

    Mon, Dec 4, 2023, 11:48 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -3,7 +3,14 @@ from PIL import Image
 def main():
 
 def check():
-    
+    ext = (".j)
+    f1, ext1 = os.path.splitext(sys.argv[1])
+    f2, ext2 = os.path.splitext(sys.argv[2])
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    elif ext1 not in
 
 
 if __name__ == "__main__":
