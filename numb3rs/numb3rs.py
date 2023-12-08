841e3bff5b94fd0730d50e99fd1693cd2c4d1a9f
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-08 21:50:23 +0000

    Fri, Dec 8, 2023, 10:50 PM +01:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,11 +10,11 @@ def validate(ip):
     match = re.search(r"^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)$", ip)
     if match:
         for i in range(4):
-            if int(match.group(i)) > 255:
+            if int(match.group(i+1)) > 255:
                 return False
             else:
                 return True
-         
+
     else:
         return False
 
