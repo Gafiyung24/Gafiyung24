96360668261262653f0bea40cda667f4b95ebd41
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-08 22:44:27 +0000

    Fri, Dec 8, 2023, 11:44 PM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,13 +1,8 @@
 import pytest
 from numb3rs import validate
-def main():
 
 def test_validate():
     assert validate("255.1.2.3") == True
     assert validate("1000.400.300.500") == False
     assert validate("cat") == False
 
-
-
-if __name__== "__main__:"
-    main
