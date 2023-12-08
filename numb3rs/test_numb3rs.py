2fe1fb6ebd3467f90de5a20a5c37c272ebe796ec
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-08 23:06:22 +0000

    Sat, Dec 9, 2023, 12:06 AM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,6 +5,6 @@ def test_validate():
     assert validate("255.1.2.3") == True
     assert validate("1000.400.300.500") == False
     assert validate("cat") == False
-    assert validate("1") == False
-    
+    assert validate("1.500.2000.1000") == False
+
 
