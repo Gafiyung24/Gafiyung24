702dba1cd5ea388f0cb37105da4e0bc0719d4acb
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-08 22:39:02 +0000

    Fri, Dec 8, 2023, 11:39 PM +01:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,8 +1,14 @@
 import pytest
+from numb3rs import validate
 def main():
 
+def test_validate():
+    assert validate("255.1.2.3") == True
+    assert validate(")
 
 
-    
+
+
+
 if __name__== "__main__:
     main
