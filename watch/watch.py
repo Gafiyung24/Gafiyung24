b11be33fe3c175da329029e5347764a6ba9b3357
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-09 01:27:16 +0000

    Sat, Dec 9, 2023, 2:27 AM +01:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,7 +7,13 @@ def main():
 
 
 def parse(s):
-    match = re.search(r"src=\"https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9]+)\"
+    match = re.search(r'src=\"https?://(?:www\.)?youtube\.com/embed/([a-zA-Z0-9]+)\"', s)
+    if match:
+        url_n = match.group(1)
+        return f"http://youtu.be/{url_n}"
+    else:
+        return None
+
 
 
 
