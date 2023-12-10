4abd2f6e34062aa36409e192f1b2f2dc7eab301a
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-10 14:16:09 +0000

    Sun, Dec 10, 2023, 3:16 PM +01:00

---
+++ working/working.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    match = re.search(r"^([0-9]+):([0-9]+))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
