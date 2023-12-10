d962a502b37c9fe154a451d526891e84c8b28fc5
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-10 23:35:06 +0000

    Mon, Dec 11, 2023, 12:35 AM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -4,4 +4,6 @@ from working import convert
 #test for pattern 5:00 AM to 5:00 PM
 def test_p1():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5:00 PM") == "09:00 to 17:00"
     
