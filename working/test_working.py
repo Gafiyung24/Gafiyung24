1812d91091a609d39b28e418f060c492c8f84706
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-10 23:48:27 +0000

    Mon, Dec 11, 2023, 12:48 AM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -7,11 +7,13 @@ def test_p1():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("9 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("9:00 AM to 5 PM") == "09:00 to 17:00"
-def test_ValueError():
+def test_ValueError1():
     with pytest.raises(ValueError):
         convert("9:00 AM to 17:00 PM")
     with pytest.raises(ValueError):
         convert("9:65 AM to 5:00 PM")
     with pytest.raises(ValueError):
         convert("9:00 AM to 5:70 PM")
-    
+def test_ValueError2():
+    with pytest.raises(ValueError):
+        convert(")
