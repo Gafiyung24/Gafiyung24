dd502579bad6900758256994cc84292c341c2d7c
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-10 15:05:13 +0000

    Sun, Dec 10, 2023, 4:05 PM +01:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     #find match for pattern 9:00 AM to 5:00PM with regex
-    match = re.search(r"^([0-9]+):([0-9]*) {1}(AM|PM) {1}to {1}([0-9]+):([0-9]*) {1}(AM|PM)$", s)
+    match = re.search(r"^([0-9]+):*([0-9]*) {1}(AM|PM) {1}to {1}([0-9]+):*([0-9]*) {1}(AM|PM)$", s)
     if match:
         #conditionals to make sure input meets specified criteria
         try:
