058e1c54c8ffe53977d73312654b3485149258c7
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-11 00:31:16 +0000

    Mon, Dec 11, 2023, 1:31 AM +01:00

--- working/working.py
+++ working/working.py
@@ -73,7 +73,7 @@ def convert(s):
                 return f"{int(match.group(1)):02}:{int(match.group(2)):02} to {int(match.group(4)):02}:00"
             elif match.group(3) == "AM" and match.group(5) == "PM":
                 return f"{int(match.group(1)):02}:{int(match.group(2)):02} to {int(match.group(4))+12}:00"
-            elif match.groups(3) == "PM" and match.group(5) == "AM":
+            elif match.group(3) == "PM" and match.group(5) == "AM":
                 return f"{int(match.group(1))+12}:{int(match.group(2)):02} to {int(match.group(4)):02}:00"
             elif match.groups(3) == "PM" and match.group(5) == "PM":
                 return f" {int(match.group(1))+12}:{int(match.group(2)):02} to {int(match.group(4))+12}:00"
