dacfae518427ab3af7bad50e8f2c565a18f8b866
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-11 23:39:42 +0000

    Tue, Dec 12, 2023, 12:39 AM +01:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,5 +1,14 @@
 from um import count
+import pytest
 
 def test_type1():
-    
+    assert count("hello, um") == 1
+    assert count("um hello, um") == 2
+    assert count("um try um, um") == 3
+def test_type2():
+    assert count("UM please") == 1
+    assert count("UM UM thanks") == 2
+def test_type3():
+    assert count("Um, UM, um") == 3
+    assert count("uM, um, UM") == 3
 
