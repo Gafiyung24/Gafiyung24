005aa629f554aaf5d8785db8604284ad4a7e6ace
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-11 00:18:31 +0000

    Mon, Dec 11, 2023, 1:18 AM +01:00

--- working/working.py
+++ working/working.py
@@ -78,7 +78,7 @@ def convert(s):
             elif match.groups(3) == "PM" and match.group(5) == "PM":
                 return f" {int(match.group(1))+12}:{int(match.group(2)):02} to {int(match.group(4))+12}:00"
 
-    elif match:= None:
+    elif match == None:
         try:
             raise ValueError
         except ValueError:
