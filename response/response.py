d9c2f7e662975140c2b512cf79d2ea0eeaac514e
Gafiyung24 <141925059+Gafiyung24@users.noreply.github.com>
2023-12-12 00:39:05 +0000

    Tue, Dec 12, 2023, 1:39 AM +01:00

--- response/response.py
+++ response/response.py
@@ -1,9 +1,15 @@
 from validator_collection import validators, checkers, errors
 
 def main():
+    print(checker(input("What's your email address? ")))
+
 
 def checker(s):
-    
+    response = checkers.is_email(s)
+    if response:
+        return valid
+    else:
+        return invalid
 
 
 
